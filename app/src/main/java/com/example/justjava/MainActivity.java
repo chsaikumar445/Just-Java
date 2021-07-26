952d67b8bd9cbfcbc65cb8788f82d44365827bf9
CH.SAIKUMAR <ch.saikumar445@gmail.com>
2021-07-26 11:43:52 +0530

    modified xml file

--- app/src/main/java/com/example/justjava/MainActivity.java
+++ app/src/main/java/com/example/justjava/MainActivity.java
@@ -2,11 +2,17 @@ package com.example.justjava;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.icu.text.NumberFormat;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    /*
+   creates a global variable and assigns value 2
+    */
+    int quantity =2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +21,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * This method is called when the order button is clicked.
+     * This method is called when the increment button is clicked.
      */
+    public void increment(View view) {
+        if (quantity >= 0) {
+            quantity = quantity + 1;
+            display(quantity);
+        }
+    }
+
+    /*
+    this method is called when the decrement is clicked
+     */
+    public void decrement(View view) {
+        if (quantity > 0) {
+            quantity = quantity - 1;
+            display(quantity);
+        }
+    }
+/*
+this method is called when the order button is clicked
+ */
     public void sendMessage(View view) {
-        display(1);
+        displayPrice(quantity * 5);
     }
 
     /**
@@ -28,6 +53,12 @@ public class MainActivity extends AppCompatActivity {
         TextView quantityTextView = (TextView) findViewById(
                 R.id.quantity_text_view);
         quantityTextView.setText("" + number);
+    }
+
+    private void displayPrice(int number){
+        TextView priceTextView = (TextView) findViewById(
+                R.id.price_text_view   );
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 
 }
